f5bce19df5f30182285c7e4efaa2a8833e391bc0
RMCV-Rajapaksha <147098368+RMCV-Rajapaksha@users.noreply.github.com>
2024-03-07 23:43:11 +0530

    Add about section with image and content

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Spline from '@splinetool/react-spline';
 import { useState } from 'react';
 import {IoMenu} from 'react-icons/io5';
 
+import May from './images/may.jpg';
+
 function App() {
   const [isActive, setIsActive] = useState(false);
   return (
-    <div className="flex w-screen h-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20">
+    <div className="flex w-screen  min-h-screen flex-col items-center justify-center relative bg-primary pb-20">
       {/*Navigation bar*/}
       <nav className='w-full px-6 z-50 fixed inset-x-0 top-2 flex justify-center items-center'>
         <div className='w-full md:w-880 bg-navBar p-4 rounded-2xl flex items-center'>
@@ -42,6 +44,27 @@ function App() {
           </div>
         </div>
       </div>
+
+      {/*Main section*/}
+
+      <main className='w-{80%} mt-5 '>
+{/*About section*/}
+<section className='w-full grid grid-cols-1 md:grid-cols-2 gap-4 my-24' id="about">
+{/*image box*/}
+<div className='w-full h-420 flex items-center justify-center'>
+<div className='w-275 h-340 bg-emerald-200 rounded-md relative'>
+  <img className='w-full h-full absolute -right-4 top-4 object-cover rounded-lg shadow-lg' src={May} alt=""/>
+</div>
+</div>
+{/*content box*/}
+<div className='w-full h-420 flex flex-col items-center justify-center'>
+<p className='text-lg text-textBase text-center'>
+Hello, coding enthusiasts! I'm R.M.C.V Rajapaksha, an aspiring computer engineering undergraduate at the University of Ruhuna, and I'm delighted to share a bit about my coding journey with you.I'm a passionate coder who believes in the limitless possibilities of creative thinking and programming knowledge. Currently immersed in the world of computer engineering at the University of Ruhuna, my journey began with a curiosity to solve problems using the power of code.
+</p>
+<button className=''></button>
+</div>
+</section>
+      </main>
     </div>
   );
 }
